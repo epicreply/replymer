eb248cc5a4d63ad94d4453422f109025b8085ba2
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-31 23:56:28 +0200

    Update AuthPage.tsx

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -104,9 +104,6 @@ const AuthPage = () => {
 
           {/* Divider */}
           <div className="relative my-6">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-border" />
-            </div>
             <div className="relative flex justify-center text-xs uppercase">
               <span className="bg-background px-2 text-muted-foreground">or continue with</span>
             </div>
